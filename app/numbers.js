070d7553e02310714c7c29c9693960f150777528
Collin Adams <collinadams04@gmail.com>
2016-04-21 19:11:13 -0700

    Finish convertToBinary

--- app/numbers.js
+++ app/numbers.js
@@ -14,7 +14,16 @@ exports.numbersAnswers = {
   },
 
   convertToBinary: function(num) {
-
+    var result = '';
+    for(var i = 7; i >= 0; i--){
+      if(num >= Math.pow(2, i)){
+        result += 1;
+        num -= Math.pow(2, i);
+      }else{
+        result += 0;
+      }
+    }
+    return result;
   },
 
   multiply: function(a, b) {
