afe74ce6514fc148362a0b38be212946bf295c36
Collin Adams <collinadams04@gmail.com>
2016-04-21 18:56:09 -0700

    Finish base10

--- app/numbers.js
+++ app/numbers.js
@@ -6,7 +6,11 @@ exports.numbersAnswers = {
   },
 
   base10: function(str) {
-
+    var result = 0; 
+    for(var i = 0; i < str.length; i++){
+      result += (Math.pow(2, str.length - 1 -i) * (+(str[i])));
+    }
+    return result;
   },
 
   convertToBinary: function(num) {
