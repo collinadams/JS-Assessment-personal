24447905434aeb88a66544c6489808c99d6d0833
Collin Adams <collinadams04@gmail.com>
2016-04-28 11:26:33 -0700

    Finish Count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var timeout;
+    var setNextTimeout = function(){
+      console.log(start++);
 
+      if(start <= end){
+        timeout = setTimeout(setNextTimeout, 100);
+      }
+    };
+
+    setNextTimeout();
+
+    return {
+      cancel: function(){
+        clearTimeout(timeout);
+      }
+    }
   }
 };
