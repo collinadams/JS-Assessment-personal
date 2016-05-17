c253eafc74bd653b6069edbb1f4e218cf93c0024
Collin Adams <collinadams04@gmail.com>
2016-05-17 11:42:50 -0700

    Finish async

--- app/async.js
+++ app/async.js
@@ -2,10 +2,30 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
+    var dfd = $.Deferred();
+    setTimeout(function(){
+      dfd.resolve(value);
+    }, 1);
+    return dfd.promise();
 
+
+    // return new Promise(function(resolve, reject){
+    //   window.setTimeout(function(){
+    //     resolve(value);
+    //   }, 1000);
+    // });
   },
 
   manipulateRemoteData : function(url) {
+    var dfd = $.Deferred();
 
+    $.ajax(url).then(function(response){
+      var names = $.map(response.people, function(person){
+        return person.name;
+      });
+      dfd.resolve(names.sort());
+    });
+
+    return dfd.promise();
   }
 };
