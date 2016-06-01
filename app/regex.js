ebb607c3a11416942b658fcf5e65742434835263
Collin Adams <collinadams04@gmail.com>
2016-06-01 10:17:04 -0700

    Regex capture 3 numbers and validate phone number

--- app/regex.js
+++ app/regex.js
@@ -14,11 +14,12 @@ exports.regexAnswers = {
   },
 
   captureThreeNumbers : function(str) {
-    
+    var matches = (/\d{3}/).exec(str);
+    return matches ? matches[0] : false;
   },
 
   matchesPattern : function(str) {
-
+    return (/^\d{3}-\d{3}-\d{4}$/).test(str);
   },
   isUSD : function(str) {
 
