7292b7ed0966c47b8cad091a9e9e92d0a01ef548
Collin Adams <collinadams04@gmail.com>
2016-06-01 09:59:11 -0700

    Regex check repeating letter and ends with vowel

--- app/regex.js
+++ app/regex.js
@@ -6,15 +6,15 @@ exports.regexAnswers = {
   },
 
   containsRepeatingLetter : function(str) {
-
+    return (/([A-Za-z])\1/).test(str);
   },
 
   endsWithVowel : function(str) {
-
+    return (/[aeiou]$/i).test(str);
   },
 
   captureThreeNumbers : function(str) {
-
+    
   },
 
   matchesPattern : function(str) {
